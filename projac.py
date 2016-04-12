bdab3ce40e4e664a1355949f0c9a75aa76228b26
Rafael Milleo Carrenho <rafaelmc@hdi.com.br>
2016-04-12 16:59:56 -0300

    Começando o código dessa bagaça (que está bem ruim por sinal)

---
+++ projac.py
@@ -0,0 +1,25 @@
+# O compilador oficial do Fausto C
+
+import sys
+import re
+
+comandos = ( 'Conta pra gente' )
+
+def abrir_arquivo(arquivo):
+	dados = open(arquivo, "r").read()
+	return dados
+
+# Aqui acontece a magia do cinema, onde o filho chora e a mãe não vê, sério
+def interpretar(conteudo):
+	if(comandos in conteudo):
+		parametro = conteudo.replace('Conta pra gente(\"', '')
+		parametro = parametro.replace('\")', '')
+		print(parametro)
+		
+def executar():
+	dados = abrir_arquivo(sys.argv[1])
+	interpretar(dados)
+
+
+
+executar()
